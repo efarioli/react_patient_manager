493e69e286c7a17aae39d11b9d4e5ef0ea628cd4
Ezequiel Farioli <efarioli@gmail.com>
2020-09-25 19:23:44 +0100

    wiring Form Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import Form from './components/Form'
 
 function App() {
-  return <h1>Patient Manager</h1>
+  return (
+    <>
+      <h1>Patient Manager</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Form />
+          </div>
+          <div className="one-half column">2</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+      <h2>From the form</h2>
+    </div>
+  )
+}
+export default Form
